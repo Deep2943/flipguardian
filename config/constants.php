e330b1c4d91e92b08d4e1635151fc3a238bc403d
Deep Suthar <suthardeep2624@gmail.com>
2024-02-15 00:02:11 +0530

    adding html files, header and banner design

--- config/constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-const SITE_URL = 'http://localhost/flipguardian/';
-const ASSETS_PATH = SITE_URL;
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php require('include/header.php') ?>
-
-<section>
-    This is home page.
-</section>
-
-<?php require('include/footer.php') ?>
